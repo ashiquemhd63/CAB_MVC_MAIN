6043690b83e938d0a47620925d9c8cf45771bf73
ashiquemhd63 <muhammedashiques2017@gmail.com>
2022-11-24 22:33:46 +0530

    configured cookie session for authentication

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const parser = require('body-parser');
 const passengerRoutes = require('./routes/passengerRoutes');
 const path = require('path');
 const {engine} = require('express-handlebars');
+const cookieSession = require('cookie-session')
+
 
 const app = express();
 app.engine('handlebars', engine());
@@ -12,6 +14,15 @@ app.set('view engine', 'handlebars');
 
 app.use("/", parser.urlencoded({extended: true}));
 app.use("/static", express.static(path.join(__dirname, 'static')));
+
+//configuring cookie session
+
+app.use(cookieSession({
+    name :'session',
+    httpOnly : true,
+    keys : ["abcttyu"],
+    maxAge : 24 * 60 * 60 * 1000 
+}))
 app.use(passengerRoutes);
 
 app.listen(80)
